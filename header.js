3f5cb0c48a474d3aca3ff417105816aab199d256
qholam <qholam@wpi.edu>
2016-11-27 21:36:48 -0500

    initial commit

---
+++ header.js
@@ -0,0 +1,24 @@
+document.write('\
+  <!--logo on top of page-->\
+  <div class="headerlogo">\
+    <p>Bike app logo here?</p>\
+  </div>\
+  \
+  <!--navigation bar-->\
+  <header>\
+    <nav>\
+        <ul>\
+          <li><a href="homepage.html">HOME</a></li>\
+          <li><a href="about.html">ABOUT</a></li>\
+          <li><a href="">MAP</a></li>\
+          <li><a href="other.html">OTHER</a></li>\
+          <li><a href="tips.html">TIPS</a></li>\
+          <li><a href="contact.html">CONTACT</a></li>\
+         </ul>\
+    </nav>\
+    <p class="app_name">App Name</p>\
+  </header>\
+  \
+  <!--picture below navigation bar-->\
+  <div class="picture"><img src="images/lake-placid-cycling.jpg" class="bikepic"></div>\
+');
